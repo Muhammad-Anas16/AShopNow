20ba8abb9482e8add5a48ece8d3dfac21fad913f
Muhammad-Anas16 <anas.hanf38@gmail.com>
2025-11-24 14:29:04 +0500

    add Footer

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
                         Reserved.
                     </span>
                     <div className="flex mt-4 sm:justify-center md:mt-0 space-x-2 rtl:space-x-reverse">
-                        <a href="#" className="text-body hover:text-heading">
+                        <Link href="#" className="text-body hover:text-heading">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -30,8 +30,8 @@ const Footer = () => {
                                 />
                             </svg>
                             <span className="sr-only">Facebook page</span>
-                        </a>
-                        <a href="#" className="text-body hover:text-heading ms-5">
+                        </Link>
+                        <Link href="#" className="text-body hover:text-heading ms-5">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -44,8 +44,8 @@ const Footer = () => {
                                 <path d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
                             </svg>
                             <span className="sr-only">Discord community</span>
-                        </a>
-                        <a href="#" className="text-body hover:text-heading ms-5">
+                        </Link>
+                        <Link href="#" className="text-body hover:text-heading ms-5">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -58,8 +58,8 @@ const Footer = () => {
                                 <path d="M13.795 10.533 20.68 2h-3.073l-5.255 6.517L7.69 2H1l7.806 10.91L1.47 22h3.074l5.705-7.07L15.31 22H22l-8.205-11.467Zm-2.38 2.95L9.97 11.464 4.36 3.627h2.31l4.528 6.317 1.443 2.02 6.018 8.409h-2.31l-4.934-6.89Z" />
                             </svg>
                             <span className="sr-only">Twitter page</span>
-                        </a>
-                        <a href="#" className="text-body hover:text-heading ms-5">
+                        </Link>
+                        <Link href="#" className="text-body hover:text-heading ms-5">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -76,8 +76,8 @@ const Footer = () => {
                                 />
                             </svg>
                             <span className="sr-only">GitHub account</span>
-                        </a>
-                        <a href="#" className="text-body hover:text-heading ms-5">
+                        </Link>
+                        <Link href="#" className="text-body hover:text-heading ms-5">
                             <svg
                                 className="w-5 h-5"
                                 aria-hidden="true"
@@ -94,7 +94,7 @@ const Footer = () => {
                                 />
                             </svg>
                             <span className="sr-only">Dribbble account</span>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
